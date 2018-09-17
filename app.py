1c7e9b427c0e958fed165ec34f00247f0a84c934
Adrian Matejov <a.matejov@centrum.sk>
2018-09-17 19:11:41 +0200

    janitoring: removed unused code and `next` command
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import json
-import datetime
 
 import imhdsk
 import cpsk
@@ -11,46 +10,27 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-searched = {}
-
-
-@app.route('/', methods=['GET'])
-def verify():
-    if "hub.challenge" in request.args:
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
-
-    return "travel bot page - privacy policy at /privacy", 200
-
-
-@app.route('/privacy', methods=['GET'])
-def privacy():
-    out = 'We do not collect any profile info, user data or cookies.'
-    return out, 200
-
 
 def send_message(recipient_id, message_text):
-
-    log("to {recipient}: {text}".format(recipient=recipient_id,
+    log('to {recipient}: {text}'.format(recipient=recipient_id,
                                         text=message_text))
 
     params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+        'access_token': os.environ['PAGE_ACCESS_TOKEN']
     }
     headers = {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
     }
     data = json.dumps({
-        "recipient": {
-            "id": recipient_id
+        'recipient': {
+            'id': recipient_id
         },
-        "message": {
-            "text": message_text
+        'message': {
+            'text': message_text
         },
-        "tag": "NON_PROMOTIONAL_SUBSCRIPTION"
+        'tag': 'NON_PROMOTIONAL_SUBSCRIPTION'
     })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    r = requests.post('https://graph.facebook.com/v2.6/me/messages',
                       params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
@@ -64,57 +44,19 @@ def split_args_by(args, by):
 def generate_output(nick, dep, dest, date, result=None):
     out = 'Nothing found'
 
-    if date == '':
-        date = datetime.datetime.today().strftime('%d.%m.%Y')
-
     if len(result) >= 1:
         out = result[0].__repr__()
-        obj = result[0]
-
-        if hasattr(obj, 'lines') and len(obj.lines) >= 1:
-            time = obj.lines[0].departure
-        elif hasattr(result[0], 'drives') and len(obj.drives) >= 1:
-            time = result[0].drives[0].begin_time
-        searched[nick] = [dep, dest, time, date]
 
     return out
 
 
-def searched_incrementer(nick):
-    dep, dest, time, date = searched[nick]
-
-    dateobj = datetime.datetime.strptime('{0}-{1}'.format(time,
-                                                          date),
-                                         '%H:%M-%d.%m.%Y')
-    dateobj += datetime.timedelta(seconds=60)
-    date = dateobj.strftime('%d.%m.%Y')
-    time = dateobj.strftime('%H:%M')
-
-    searched[nick] = [dep, dest, time, date]
-    return searched[nick]
-
-
-def rootify(self, word):
-    if len(word) <= 5:
-        return word
-
-    w = word[::-1]
-    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-    for x in range(len(word)):
-        if w[x] in vowels and x != 0:
-            return word[:-(x + 1)]
-
-
+# taken from
+# https://github.com/errbotters/err-sktravel/blob/master/sktravel.py#L70
 def mhd(nick, args):
     if '-' in args:
         args = split_args_by(args, '-')
     else:
         args = args.split(' ')
-
-    if len(args) >= 1 and args[0] == 'next':
-        if nick not in searched:
-            return 'No next line'
-        args = searched_incrementer(nick)
 
     if len(args) < 2:
         return 'Not enough arguments specified. See help for usage'
@@ -143,11 +85,6 @@ def get_line(nick, args, vehicle):
     else:
         args = args.split(' ')
 
-    if len(args) >= 1 and args[0] == 'next':
-        if nick not in searched:
-            return 'No next line'
-        args = searched_incrementer(nick)
-
     if len(args) < 2:
         return 'Not enough arguments specified. See help for usage'
 
@@ -167,16 +104,17 @@ def get_line(nick, args, vehicle):
 def response(sender_id, msg):
     out = 'Wrong command. Type help.'
     try:
-        if msg.lower().startswith('vlak '):
+        lower = msg.lower()
+        if lower.startswith('vlak '):
             out = get_line(sender_id, msg[5:], 'vlak')
-        elif msg.lower().startswith('bus '):
+        elif lower.startswith('bus '):
             out = get_line(sender_id, msg[4:], 'bus')
-        elif msg.lower().startswith('spoj '):
+        elif lower.startswith('spoj '):
             out = get_line(sender_id, msg[5:], 'vlakbus')
-        elif msg.lower().startswith('mhd '):
+        elif lower.startswith('mhd '):
             out = mhd(sender_id, msg[4:])
-        elif msg.lower().startswith('help'):
-            out = 'vlak/bus/mhd/spoj <from> - <to> - [time] - [date]\nor vlak/bus/mhd/spoj next'
+        elif lower.startswith('help'):
+            out = 'vlak/bus/mhd/spoj <from> - <to> - [time] - [date]'
     except:
         out = 'Failed to retrieve data.'
 
@@ -188,17 +126,33 @@ def webhook():
     data = request.get_json()
     log(data)
 
-    if data["object"] == "page":
-        for entry in data["entry"]:
-            if "messaging" in entry:
-                for messaging_event in entry["messaging"]:
-                    if messaging_event.get("message"):
-                        sender_id = messaging_event["sender"]["id"] 
-                        message_text = messaging_event["message"]["text"]
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            if 'messaging' in entry:
+                for messaging_event in entry['messaging']:
+                    if messaging_event.get('message'):
+                        sender_id = messaging_event['sender']['id']
+                        message_text = messaging_event['message']['text']
 
                         response(sender_id, message_text)
 
     return 'ok', 200
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    if 'hub.challenge' in request.args:
+        if not request.args.get('hub.verify_token') == os.environ['VERIFY_TOKEN']:
+            return 'Verification token mismatch', 403
+        return request.args['hub.challenge'], 200
+
+    return 'travel bot page - privacy policy at /privacy', 200
+
+
+@app.route('/privacy', methods=['GET'])
+def privacy():
+    out = 'We do not collect any profile info, user data or cookies.'
+    return out, 200
 
 
 def log(message):
